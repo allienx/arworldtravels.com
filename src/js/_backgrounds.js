a728823f9269c9a6dd4075d137ec911330776414
Alex Lin <alexlinpm@gmail.com>
2018-05-11 08:37:08 -0700

    Optimize and fix background slideshow transitions by using opacity

--- src/js/_backgrounds.js
+++ src/js/_backgrounds.js
@@ -1,12 +1,25 @@
+/**
+ * This background slideshow is implemented with two div elements. They are positioned absolutely
+ * so that one is on top of the other (the bottom element is not visible initially, opacity: 0).
+ * When the bottom element changes to an opacity of 1, it becomes visible because it was defined
+ * after the top element. The switching of images is done by cycling the bottom element's opacity
+ * between 0 and 1 on a timer. A transition is defined on the bottom element's opacity property.
+ * The "hidden" element will always load the next image in the slideshow so that we don't have
+ * to preload all the images.
+ */
+
 import $ from "jquery";
 
 export default {
   init
 };
 
-const header = $("header.masthead");
-const timerLengthMS = 5000;
+const topSlide = $(".background-slideshow.top");
+const bottomSlide = $(".background-slideshow.bottom");
+const timerLengthMS = 6000;
+const transitionDurationMS = 1300;
 
+let isTopSlideShowing = true;
 let timerID = null;
 let usedUrls = [];
 let urls = [
@@ -17,16 +30,15 @@ let urls = [
   "/assets/img/background-hoi-an.jpg",
   "/assets/img/background-lucca.jpg",
   "/assets/img/background-matterhorn.jpg",
-  "/assets/img/background-milford-sound.jpg"
+  "/assets/img/background-milford-sound.jpg",
+  "/assets/img/background-manarola.jpg"
 ];
+
+topSlide.css("background-image", "url(" + urls.pop() + ")");
+bottomSlide.css("background-image", "url(" + getRandomUrl() + ")");
 
 function init() {
   timerID = setInterval(changeBackground, timerLengthMS);
-
-  // pre-load background images for a smooth transition
-  for (let i = urls.length - 1; i >= 0; i--) {
-    new Image().src = urls[i];
-  }
 }
 
 function changeBackground() {
@@ -35,12 +47,23 @@ function changeBackground() {
     usedUrls = [];
   }
 
-  let index = getRandomInt(0, urls.length);
-  let newUrl = urls[index];
+  let nextSlide = isTopSlideShowing ? topSlide : bottomSlide;
+
+  bottomSlide.css("opacity", isTopSlideShowing ? "1" : "0");
+
+  // Wait until the transition is finished so the loading of the next image isn't visible.
+  setTimeout(() => nextSlide.css("background-image", "url(" + getRandomUrl() + ")"), transitionDurationMS);
+
+  isTopSlideShowing = !isTopSlideShowing;
+}
+
+function getRandomUrl() {
+  const index = getRandomInt(0, urls.length);
+  const url = urls[index];
 
   usedUrls = usedUrls.concat(urls.splice(index, 1));
 
-  header.css("background-image", "url(" + newUrl + ")");
+  return  url;
 }
 
 // returns an integer, min <= integer < max
--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,14 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 
+import Backgrounds from "./_backgrounds.js";
 import NavigationDrawer from "./_navigation-drawer.js";
 import RachsBeans from "./_rachs-beans.js";
 import DayNight from "./_daynight.js";
 import HoverHacks from "./_hover-hacks.js";
-import Backgrounds from "./_backgrounds.js";
 
+Backgrounds.init();
 NavigationDrawer.init();
 RachsBeans.init();
 DayNight.init();
 HoverHacks.init();
-Backgrounds.init();
